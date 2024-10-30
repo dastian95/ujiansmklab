c3ed5774d70a59538d49474456f48e2ab78808d8
Aufa <aufadzwicaksono@gmail.com>
2024-10-30 15:13:19 +0700

    aufa pulang

--- application/views/_templates/auth/_header.php
+++ application/views/_templates/auth/_header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Login</title>
+    <title>TEST</title>
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
     <?php $logo_app = $setting->logo_kiri == null ? base_url() . 'assets/img/favicon.png' : base_url() . $setting->logo_kiri; ?>
